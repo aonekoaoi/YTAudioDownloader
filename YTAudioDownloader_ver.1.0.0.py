4f674335099fcb669f2f82a362b7b40618d1432b
aonekoaoi <132233257+aonekoaoi@users.noreply.github.com>
2023-08-11 23:26:10 +0900

    Add the first data.

---
+++ YTAudioDownloader_ver.1.0.0.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import ffmpeg
+import pytube
+
+print("\n著作権は著作者に帰属します。そのためデータの取り扱いには注意してください。")
+
+print("\nYouTubeのURLをコピーしコンソールにペースト後、Enterで確定してください。")
+url = str(input(">>"))
+stream = pytube.YouTube(url)
+
+# outputフォルダーをデスクトップに作成
+desktop_path = os.path.expanduser("~/Desktop")
+folder_path = os.path.join(desktop_path, "output")
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+    print("\noutputフォルダーをデスクトップに作成しました。")
+else:
+    print("\noutputフォルダーがデスクトップに存在しています。そのためoutputフォルダーをデスクトップに作成できません。")
+
+
+def audio_fun(bps):
+    """
+    音声ビットレートの情報取得動作
+    bps: 音声ビットレートを代入
+    """
+    print(bps, "の音声ビットレートの情報取得中")
+    audio = stream.streams.filter(abr=bps).first()
+    return audio
+
+
+print("\n音声ビットレートの情報取得開始")
+audio = audio_fun("256kbps")
+if audio is None:
+    audio = audio_fun("160kbps")
+elif audio is None:
+    audio = audio_fun("128kbps")
+elif audio is None:
+    audio = audio_fun("70kbps")
+elif audio is None:
+    audio = audio_fun("50kbps")
+elif audio is None:
+    audio = stream.streams.get_audio_only() # 上記の処理で音声ビットレートが存在しない場合の処理
+else:
+    print("音声ビットレートの情報取得ができません。\nお手数ですがYouTubeのURLかを確認した上で再度、プログラムを開始してください。\n")
+    sys.exit() # プログラムの強制終了
+print("音声ビットレートの情報取得終了")
+
+print("\nダウンロードの開始")
+audio.download(folder_path, "audio.mp4a")
+print("ダウンロードの終了")
+
+print("\nフォーマットの変更開始")
+audio_path = os.path.join(folder_path, "audio.mp4a")
+audio_input = ffmpeg.input(audio_path)
+complete = os.path.join(folder_path, f"{stream.title}.mp3")
+ffmpeg.output(audio_input, complete, format="mp3").run(capture_stderr=True)
+print("フォーマットの変更終了")
+
+os.remove(audio_path) # .mp4a形式のファイルを削除
+
+print("\nデスクトップのoutputフォルダー内に「", f"{stream.title}.mp3 」が格納されています。\n")
